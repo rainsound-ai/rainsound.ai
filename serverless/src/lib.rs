76a79895010686a4c1674fbe0afb32735162369b
Luca Bailey <mail@photon.garden>
2023-09-16 10:29:21 -0600

    Deploy.

--- serverless/src/lib.rs
+++ serverless/src/lib.rs
@@ -2,18 +2,46 @@ use worker::*;
 
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
-    console_log!(
-        "{} {}, located at: {:?}, within: {}",
-        req.method().to_string(),
-        req.path(),
-        req.cf().coordinates().unwrap_or_default(),
-        req.cf().region().unwrap_or("unknown region".into())
-    );
+    // console_log!(
+    //     "{} {}, located at: {:?}, within: {}",
+    //     req.method().to_string(),
+    //     req.path(),
+    //     req.cf().coordinates().unwrap_or_default(),
+    //     req.cf().region().unwrap_or("unknown region".into())
+    // );
 
-    return Response::ok("Hello, world!").map(|mut r| {
-        r.headers_mut().set("Content-Type", "text/plain");
-        r
-    });
+    Router::new()
+        .get("/", |_req, _ctx| {
+            let html = include_str!("../../built/index.html");
+            Response::from_html(html)
+        })
+        .get("/main.css", |_req, _ctx| {
+            let css = include_str!("../../built/main.css");
+            Response::ok(css).map(|mut r| {
+                r.headers_mut().set("Content-Type", "text/css").unwrap();
+                r
+            })
+        })
+        .get("browser.js", |_req, _ctx| {
+            let js = include_str!("../../built/browser.js");
+            Response::ok(js).map(|mut r| {
+                r.headers_mut()
+                    .set("Content-Type", "text/javascript")
+                    .unwrap();
+                r
+            })
+        })
+        .get("browser_bg.wasm", |_req, _ctx| {
+            let wasm = include_bytes!("../../built/browser_bg.wasm").to_vec();
+            Response::from_bytes(wasm).map(|mut r| {
+                r.headers_mut()
+                    .set("Content-Type", "application/wasm")
+                    .unwrap();
+                r
+            })
+        })
+        .run(req, env)
+        .await
 
     // if !matches!(req.method(), Method::Post) {
     //     return Response::error("Method Not Allowed", 405);
@@ -27,6 +55,4 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     //         _ => Response::error("`file` part of POST form must be a file", 400),
     //     };
     // }
-
-    Response::error("Bad Request", 400)
 }
