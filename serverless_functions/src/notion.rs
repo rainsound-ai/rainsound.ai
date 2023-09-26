6f90a7a7c6dffffd2b9679452794fb68da7b2da4
iandoesallthethings <iandoesallthethings@gmail.com>
2023-09-26 12:15:14 -0400

    Tweak secrets.

--- serverless_functions/src/notion.rs
+++ serverless_functions/src/notion.rs
@@ -1,29 +1,32 @@
-// use crate::ContactFormSubmission;
-// use notion::models::PageCreateRequest;
+use crate::ContactFormSubmission;
+use notion::models::PageCreateRequest;
+use std::env;
 
-// pub async fn add_to_database(form_data: ContactFormSubmission) {
-//     let properties = hashmap! {
-//         name: form_data.name,
-//         email: form_data.email,
-//         message: form_data.message,
-//     };
+pub async fn add_contact_form_submission_to_database(form_data: ContactFormSubmission) {
+    let properties = hashmap! {
+        name: form_data.name,
+        email: form_data.email,
+        message: form_data.message,
+    };
 
-//     let page = PageCreateRequest {
-//         parent: get_database_id(),
-//         properties,
-//     };
+    let page = PageCreateRequest {
+        parent: get_database_id(),
+        properties,
+    };
 
-//     notion_api.create_page(page).await.unwrap();
-// }
+    notion_api.create_page(page).await.unwrap();
+}
 
-// fn get_api() {
-//     let api_token = std::env::var("NOTION_API_TOKEN");
+fn get_api() {
+    let api_token = env::var("NOTION_API_TOKEN");
+    dbg!(&api_token);
 
-//     NotionApi::new(api_token)
-// }
+    NotionApi::new(api_token)
+}
 
-// fn get_database_id() {
-//     let database_id = std::env::var("NOTION_DATABASE_ID");
+fn get_database_id() {
+    let database_id = env::var("NOTION_DATABASE_ID");
+    dbg!(&database_id);
 
-//     Parent::DatabaseId(DatabaseId::new(database_id))
-// }
+    Parent::DatabaseId(DatabaseId::new(database_id))
+}
