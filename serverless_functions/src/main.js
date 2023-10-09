01ee2aa7485d197d9060357a521a1feeddc53044
Luca Bailey <mail@photon.garden>
2023-10-09 08:25:02 -0700

    Tweak comment formatting.

--- serverless_functions/src/main.js
+++ serverless_functions/src/main.js
@@ -1,7 +1,9 @@
 // This file imports and boots the JS wrapper around our wasm code.
 // If we exported specific functions (like `greet`) from our wasm code,
 // we could import them like this:
+//
 // import init, { greet } from './browser.js'
+//
 // The `from` part of this import gets replaced with the actual filename
 // when we include this file in the HTML.
 import wasmMain from './{browser_js_filename}'
