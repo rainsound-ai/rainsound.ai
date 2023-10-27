27c85b2712d24e250e5e39a8a3205de0f6db87a2
Luca Bailey <mail@photon.garden>
2023-10-27 13:19:20 -0400

    Fix `spin watch` endlessly restarting.

--- serverless_functions/src/lib.rs
+++ serverless_functions/src/lib.rs
@@ -4,7 +4,7 @@ use spin_sdk::{
     http_component,
 };
 
-use new_assets::non_html_assets_by_path;
+// use new_assets::non_html_assets_by_path;
 // use routes::contact::form_submission::ContactFormSubmission;
 
 // mod notion;
@@ -12,12 +12,12 @@ use new_assets::non_html_assets_by_path;
 
 #[http_component]
 fn main(_req: Request) -> Result<Response> {
-    let body = new_assets::non_html_assets.hasui_hero.bytes.into();
+    let body = "Hello, static html!".into();
 
     let response = http::Response::builder()
         .status(200)
-        .header("Content-Type", "image/jpeg")
-        // .header("Content-Type", "text/html")
+        // .header("Content-Type", "image/jpeg")
+        .header("Content-Type", "text/html")
         .body(Some(body))?;
 
     Ok(response)
