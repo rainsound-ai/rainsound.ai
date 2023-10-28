22ca573266d3c7923a09ac50818270c79d39ca1c
Luca Bailey <mail@photon.garden>
2023-10-28 14:00:15 -0400

    Remove Link struct.

--- serverless_functions/src/routes/components/link.rs
+++ serverless_functions/src/routes/components/link.rs
@@ -1,5 +1,3 @@
-use std::fmt::Display;
-
 use crate::routes::Route;
 use maud::{html, Markup};
 
@@ -9,19 +7,5 @@ pub fn link<'class>(class: impl Into<&'class str>, route: Route, body: Markup) -
         a href=(route.to_string()) class=(class) {
             (body)
         }
-    }
-}
-
-pub struct Link {
-    pub class: &'static str,
-    pub route: Route,
-    pub body: Markup,
-}
-
-impl Display for Link {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        link(self.class, self.route.clone(), self.body.clone())
-            .into_string()
-            .fmt(f)
     }
 }
