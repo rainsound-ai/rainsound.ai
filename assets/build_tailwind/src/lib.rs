6f383ce35be471c9df996092ff892bc09cd8db09
Luca Bailey <mail@photon.garden>
2023-11-18 17:02:08 -0500

    Convert build_tailwind! to use the log crate.

--- assets/build_tailwind/src/lib.rs
+++ assets/build_tailwind/src/lib.rs
@@ -13,23 +13,20 @@ use syn::{
 pub fn build_tailwind(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as BuildTailwindInput);
 
-    eprintln!("[build_tailwind] Building Tailwind.");
+    log::info!("Building Tailwind.");
     let build_tailwind_dir = workspace_root_dir().join("assets").join("build_tailwind");
 
     let config = build_tailwind_dir.join("tailwind.config.js");
     let config_str = config
         .to_str()
         .expect("Error converting the path to the Tailwind config to a string.");
-    eprintln!("[build_tailwind] Using Tailwind config at {}.", config_str);
+    log::info!("Using Tailwind config at {}.", config_str);
 
     let input_file = workspace_root_dir().join(&input.path_to_input_file);
     let input_file_str = input_file
         .to_str()
         .expect("Error converting the path to the Tailwind input CSS file to a string.");
-    eprintln!(
-        "[build_tailwind] Using Tailwind input CSS file at {}.",
-        input_file_str
-    );
+    log::info!("Using Tailwind input CSS file at {}.", input_file_str);
 
     if !input_file.exists() {
         let error_message = format!("File not found: {}", input_file_str);
@@ -43,10 +40,7 @@ pub fn build_tailwind(input: TokenStream) -> TokenStream {
     let output_file_str = output_file
         .to_str()
         .expect("Error converting the path to the Tailwind output CSS file to a string.");
-    eprintln!(
-        "[build_tailwind] Saving Tailwind output to {}.",
-        output_file_str
-    );
+    log::info!("Saving Tailwind output to {}.", output_file_str);
 
     let mut run_tailwind = Command::new("npx");
 
@@ -60,13 +54,13 @@ pub fn build_tailwind(input: TokenStream) -> TokenStream {
         run_tailwind.arg("--minify");
     }
 
-    eprintln!("[build_tailwind] Invoking Tailwind CLI.");
+    log::info!("Invoking Tailwind CLI.");
     let tailwind_cli_output = run_tailwind
         .output()
         .expect("Error invoking the Tailwind CLI.");
 
     if tailwind_cli_output.status.success() {
-        eprintln!("[build_tailwind] Successfully built Tailwind.");
+        log::info!("Successfully built Tailwind.");
     } else {
         let stdout = String::from_utf8(tailwind_cli_output.stdout)
             .expect("Error converting the Tailwind CLI's output to a string.");
@@ -76,7 +70,7 @@ pub fn build_tailwind(input: TokenStream) -> TokenStream {
             "Error building Tailwind.\nstdout:\n{}\n\nstderr:\n{}",
             stdout, stderr
         );
-        eprintln!("[build_tailwind] {}", error_message);
+        log::error!("{}", error_message);
         return syn::Error::new(input.span, error_message)
             .to_compile_error()
             .into();
