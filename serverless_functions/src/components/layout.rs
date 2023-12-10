00e38e5fc6c2e1450d29a869e0af7eed5b578bcf
Luca Bailey <mail@photon.garden>
2023-12-10 16:36:21 -0500

    Remove dbg! calls.

--- serverless_functions/src/components/layout.rs
+++ serverless_functions/src/components/layout.rs
@@ -67,9 +67,7 @@ fn main_js() -> Markup {
         .full_url_path
         .to_string_lossy()
         .to_string();
-    dbg!(&browser_js_path);
     let contents = include_str!("../main.js").replace("{browser_js_filename}", &browser_js_path);
-    dbg!(&contents);
     html! {
         script type="module" {
             (PreEscaped(contents))
